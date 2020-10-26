74ba9b9cca706166225e1891fbae8b17f823d650
ProjetWEB2 Tchimtchoua Champlain <tcch1201@iepscfucclebe.onmicrosoft.com>
2020-10-26 19:02:18 +0100

    tp7

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // Écrivez votre code JavaScript ici.
+
+const ul =  document.querySelector('ul') ;
+ const  ajouter  =  document.querySelector ( 'input' ) ;
+ const bouton =  document.querySelector('button') ;
+
+ bouton . onclick  =  function ( )  {
+
+  let valuecurrent  =  ajouter.value ;
+
+  ajouter.value  =  "" ;
+
+ const li = document.createElement('li');
+ const span = document.createElement('span');
+ const button = document.createElement('button');
+
+ li. appendChild ( span) ;
+ li. appendChild ( button ) ;
+
+ span.textContent= valuecurrent;
+ button.textContent= "supprimer";
+
+ ul.appendChild(li);
+
+
+ button. onclick  =  function(){
+ ul . removeChild ( li ) ;
+input . focus ( ) ;
+ }
+ input . focus ( ) ;
+}
+
+
